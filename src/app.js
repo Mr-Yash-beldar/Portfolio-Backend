c371ca7d25f4b2ee44d9d3ea8131e9a28fef649b
Yashodip Beldar <106594839+Mr-Yash-beldar@users.noreply.github.com>
2025-11-07 20:21:31 +0530

    added domain

--- src/app.js
+++ src/app.js
@@ -32,7 +32,7 @@ app.use(helmet());
 
 // OR specify options for CORS
 app.use(cors({
-  origin: 'https://www.yashodipbeldar.engineer', // Allow requests from this origin
+  origin: 'https://yashodip.developerverse.tech', // Allow requests from this origin
   methods: ['GET', 'POST', 'PUT', 'DELETE'], // Specify allowed methods
   allowedHeaders: ['Content-Type', 'Authorization'], // Specify allowed headers
   credentials: true // Allow credentials (if necessary)
